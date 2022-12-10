e271af28525cba98a91cd5518c7fd8c11ae650b7
Moji <m0ji19881122@gmail.com>
2022-12-10 11:38:48 +0900

    Initial Commit

---
+++ delegateCollections/delegateCollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  delegateCollectionsApp.swift
+//  delegateCollections
+//
+//  Created by MacBook Air M1 on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct delegateCollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
